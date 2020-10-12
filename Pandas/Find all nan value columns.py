c44b5319792c65f0dfe689809cb628862ab2cd90
lawchihwei <luozhiwei@live.cn>
2020-10-12 11:06:55 +0800

    small amendent

--- Pandas/Find all nan value columns.py
+++ Pandas/Find all nan value columns.py
@@ -1,9 +1,6 @@
 import pandas as pd
-
 path=r"C:\Users\luoz14\Desktop\qc.sas7bdat"
-
 data=pd.read_sas(path,encoding='latin1')
-
 a=data.isnull().sum()[data.isnull().sum()==len(data)].index
 b=" ".join(list(a))
 
