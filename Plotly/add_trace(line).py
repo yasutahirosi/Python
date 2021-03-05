7a34c52b57d76a312f7eab6720524e24632329d3
lawchihwei <luozhiwei@live.cn>
2021-03-05 13:40:56 +0800

    update

---
+++ Plotly/add_trace(line).py
@@ -0,0 +1,8 @@
+fig.add_shape(type='line',
+                x0=0,
+                y0=y_test.mean()+3,
+                x1=152,
+                y1=y_test.mean()+3,
+                line=dict(color='Red',),
+                xref='x',
+                yref='y')
