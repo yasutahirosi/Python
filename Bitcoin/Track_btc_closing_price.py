2e9b24050c3241918ae034bffa224ab3ab9731ed
Minamoto Yasuta <luozhiwei@live.cn>
2021-05-22 15:09:21 +0800

    Add log picture

--- Bitcoin/Track_btc_closing_price.py
+++ Bitcoin/Track_btc_closing_price.py
@@ -1,13 +1,10 @@
-#Module----------------------------------------------------------------
+# Module----------------------------------------------------------------
 import yfinance as yf
-from sqlalchemy import create_engine
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-plt.rcParams['figure.dpi'] = 600
-plt.rcParams['savefig.dpi'] = 600
+import plotly.graph_objects as go
+from plotly.offline import plot
+from plotly.subplots import make_subplots
+
 
 # Get Bitcoin data-----------------------------------------------------
 data = yf.download(tickers='BTC-USD',
@@ -15,32 +12,41 @@ data = yf.download(tickers='BTC-USD',
                    # use "period" instead of start/end
                    # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
                    # (optional, default is '1mo')interval = '1d')
-                   interval='1d'
+                   interval='1m'
                    # fetch data by interval (including intraday if period < 60 days)
                    # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
                    # (optional, default is '1d'）
-)
-# Insert data to Mysql--------------------------------------------------
-#Set up connect
-db = 'Crypto'
-user = 'root'
-password = 'ly113216'
-engine = create_engine('mysql+pymysql://{}:{}@localhost/{}'.format(user,password,db))
-
-#To database
-data.to_sql(name='btc_1d',con=engine,if_exists='append',index=False)
+                   )
 
 # Count pct_change
 data['close_change'] = data.Close.pct_change()
-data=data.iloc[1:,:]
-correction=data[data['close_change']<=-0.1]
+data = data.iloc[1:, :]
+correction = data[data['close_change'] <= -0.1]
 correction.reset_index(inplace=True)
 correction['date_range'] = correction.Date.diff().dt.days
-correction.fillna(0,inplace=True)
+correction.fillna(0, inplace=True)
 
-#Analysis
-fig,axs = plt.subplots(ncols=2)
 
-sns.scatterplot(data=correction,x='Date',y='date_range',ax=axs[0])
+# Plot
 
-sns.lineplot(data=data,x=data.index,y='Close',ax=axs[1])
+# Create figure with secondary y-axis
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+# Add traces
+fig.add_trace(
+    go.Scatter(
+        x=correction['Date'],
+        y=correction['Close'],
+        mode='lines+markers'),
+    secondary_y=True)
+
+
+fig.add_trace(
+    go.Scatter(x=data.index,
+               y=data.Close,
+               mode='lines'),
+    secondary_y=True)
+
+fig.update_yaxes(type="log")
+
+plot(fig)
