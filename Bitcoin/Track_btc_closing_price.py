3308b09a83d6ec48d9ddbe28a4fa3053f0917959
Minamoto Yasuta <luozhiwei@live.cn>
2021-05-24 21:14:29 +0800

    ...

--- Bitcoin/Track_btc_closing_price.py
+++ Bitcoin/Track_btc_closing_price.py
@@ -12,7 +12,7 @@ data = yf.download(tickers='BTC-USD',
                    # use "period" instead of start/end
                    # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
                    # (optional, default is '1mo')interval = '1d')
-                   interval='1m'
+                   interval='1d'
                    # fetch data by interval (including intraday if period < 60 days)
                    # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
                    # (optional, default is '1d'）
